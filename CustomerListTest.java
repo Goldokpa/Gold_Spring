0e364f4c69cdef05bce19ae15ac45e839cb3d300
alen130 <alenvarghese90@gmail.com>
2023-03-30 19:44:19 +0000

    updated customerTest and CustomerListTest using Junit5

---
+++ CustomerListTest.java
@@ -0,0 +1,80 @@
+package src;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CustomerListTest {
+    private CustomerList customerList;
+    private Customer c1;
+    private Customer c2;
+    private Customer c3;
+
+    @BeforeEach
+    void setUp()throws Exception {
+        customerList = new CustomerList();
+        c1 = new Customer("John Dave", 1);
+        c2 = new Customer("Helen Paul", 2);
+        c3 = new Customer("Joy Saint", 3);
+    }
+
+    @AfterEach
+    void tearDown()throws Exception {
+        customerList = null;
+        c1 = null;
+        c2 = null;
+        c3 = null;
+    }
+
+    @Test
+    void testAddCustomer() {
+        customerList.addCustomer(c1);
+        assertEquals(1, customerList.getCustomers().size());
+        assertTrue(customerList.getCustomers().contains(c1));
+
+        customerList.addCustomer(c2);
+        assertEquals(2, customerList.getCustomers().size());
+        assertTrue(customerList.getCustomers().contains(c2));
+    }
+
+    @Test
+    void testRemoveCustomer() {
+        customerList.addCustomer(c1);
+        customerList.addCustomer(c2);
+        customerList.addCustomer(c3);
+
+        customerList.removeCustomer(c2);
+        assertEquals(2, customerList.getCustomers().size());
+        assertFalse(customerList.getCustomers().contains(c2));
+    }
+
+    @Test
+    void testGetCustomers() {
+        customerList.addCustomer(c1);
+        customerList.addCustomer(c2);
+        customerList.addCustomer(c3);
+
+        List<Customer> customers = customerList.getCustomers();
+        assertEquals(3, customers.size());
+        assertTrue(customers.contains(c1));
+        assertTrue(customers.contains(c2));
+        assertTrue(customers.contains(c3));
+    }
+
+    @Test
+    void testSetCustomers() {
+        ArrayList<Customer> newCustomers = new ArrayList<>();
+        newCustomers.add(c1);
+        newCustomers.add(c2);
+
+        customerList.setCustomers(newCustomers);
+        assertEquals(2, customerList.getCustomers().size());
+        assertTrue(customerList.getCustomers().contains(c1));
+        assertTrue(customerList.getCustomers().contains(c2));
+    }
+}
