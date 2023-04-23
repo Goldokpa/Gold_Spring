91f84af989c6c9ec535b6d618ce5d235e78a3810
Airat Yusuff <airahyusuff@gmail.com>
2023-04-23 23:46:44 +0100

    feat: updated controller to reflect threading

--- src/controllers/AppStartController.java
+++ src/controllers/AppStartController.java
@@ -2,6 +2,8 @@ package controllers;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DecimalFormat;
 import java.util.List;
 
 import javafx.collections.FXCollections;
@@ -13,7 +15,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.beans.property.ReadOnlyStringWrapper;
-
+import src.CollectionQueue;
 import src.CustomerList;
 import src.DataHandler;
 import src.DepotWorker;
@@ -51,31 +53,33 @@ public class AppStartController {
 	@FXML
 	private TableColumn<Parcel, String> dimensions;
 	
-	ObservableList<ParcelClaim> queue;
-	ObservableList<Parcel> parcels;
+	ObservableList<ParcelClaim> queueView; //for easy updating of the queue view
+	ObservableList<Parcel> parcelsView; //for easy updating of the parcels view
+	
+	private CollectionQueue cQueue;
+	private DepotWorker worker1;
 	
 
 	public void initialize() {
 		try {
+			
 			DataHandler dh = new DataHandler("./parcels.csv", "./customers.csv");
 
-			CustomerList cList = new CustomerList();
-			ParcelList pList = new ParcelList();
+			new CustomerList();
+			new ParcelList();
+			cQueue = new CollectionQueue();
 			
 			//create customers and parcels and add them to their lists
-			dh.importAllCustomers(cList);
+			dh.importAllCustomers();
 			
-			dh.importAllParcels(pList);
-			parcels = FXCollections.observableArrayList(pList.getUncollectedParcels());
-
+			dh.importAllParcels();
+			parcelsView = FXCollections.observableArrayList(ParcelList.getUncollectedParcels());
 			
-			//initialise a depot worker with the depot's list of parcels
-			DepotWorker worker1 = new DepotWorker(pList);
-
 			//create customer claims/collection queue
-			dh.createCollectionQueueForWorker("./customers.csv", worker1);
-			queue = FXCollections.observableArrayList(worker1.getCollectionQueue());
+			dh.createCollectionQueue(cQueue);
 			
+			queueView = FXCollections.observableArrayList(cQueue.getQueue());
+
 			showCollectionQueue();
 			showParcelsInventory();
 			
@@ -90,9 +94,26 @@ public class AppStartController {
 		}
 	}
 	
-	public void onClickStart(ActionEvent e) throws IOException {
+	public void startProgramListener(ActionEvent e) throws IOException {
 		System.out.println("Program should start now");
 		
+		//initialise a depot worker with the depot's list of parcels
+		worker1 = new DepotWorker(cQueue);
+			
+		//create thread for worker to start attending to the queue
+		new Thread(worker1).start();
+		
+		//create thread for customers joining the queue after worker has started attending to the queue
+		new Thread(cQueue).start();	
+		
+	}
+	
+	public void stopProgramListener(ActionEvent e) throws IOException {
+		System.out.println("Program should stop now");
+		
+		// need to figure out how to stop the threads
+		
+		endProcessing();
 	}
 	
 	public void showCollectionQueue() {
@@ -104,7 +125,7 @@ public class AppStartController {
 		    String joinedParcelIds = String.join(", ", parcelIds);
 		    return new ReadOnlyStringWrapper(joinedParcelIds);
 		});
-		collectionTable.setItems(queue);
+		collectionTable.setItems(queueView);
 	}
 	
 	public void showParcelsInventory() {
@@ -117,9 +138,44 @@ public class AppStartController {
 		    String mergedValue = (int)item.getLength() + " x " + (int)item.getWidth() + " x " + (int)item.getHeight();
 		    return new ReadOnlyStringWrapper(mergedValue);
 		});
-		allUncollectedParcels.setItems(parcels);
+		allUncollectedParcels.setItems(parcelsView);
 	}
 	
+	public void endProcessing() {
+		try {
+			// print results
+			System.out.println("\n\nDone...");
 
+			System.out.println("Number of collected parcels: " + ParcelList.getCollectedParcels().size());
+			System.out.println("Number of remaining parcels: " + ParcelList.getUncollectedParcels().size());
+
+			// store results in txt file
+			PrintWriter writer = new PrintWriter("output.txt"); // correct dir. path
+			writer.print("Depot Worker results \n\n");
+			writer.append("Number of collected parcels: " + ParcelList.getCollectedParcels().size() + "\n"); // appends to
+																										// the file
+			writer.append("Number of remaining parcels: " + ParcelList.getUncollectedParcels().size() + "\n");
+
+			DecimalFormat dpFormatter = new DecimalFormat("0.00");
+
+			double totalFees = 0;
+			writer.append("\nCollected Parcels - Collection Fee\n");
+			for (Parcel p : ParcelList.getCollectedParcels()) {
+				writer.append(p.getParcelId() + " - £" + dpFormatter.format(p.getCollectionFee()) + "\n");
+				totalFees += p.getCollectionFee();
+			}
+			writer.append("\nUncollected Parcels\n");
+
+			for (Parcel p : ParcelList.getUncollectedParcels()) {
+				writer.append(p.getParcelId() + "\n");
+			}
+
+			writer.append("\nTotal Collection Fee for the day: £" + dpFormatter.format(totalFees) + "\n");
+
+			writer.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Files may not exist");
+		}
+	}
 
 }
